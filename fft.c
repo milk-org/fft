40142e05b945738350ab7ce3ce886d6540b4f8c6
oguyon <oliv.guyon@gmail.com>
2017-12-28 12:25:38 -1000

    msync

--- fft.c
+++ fft.c
@@ -195,7 +195,12 @@ int_fast8_t fft_correlation_cli()
 void __attribute__ ((constructor)) libinit_fft()
 {
 	init_fft();
-//	printf(" ...... Loading module %s\n", __FILE__);
+
+	if(data.progStatus>0)
+	{
+		printf("  Found unloaded shared object in ./libs/ -> LOADING module %s\n", __FILE__);
+		fflush(stdout);
+	}	
 }
 
 
